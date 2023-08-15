0c33f77950c2cdb5ccade159bda449cbfc60411a
sanphet6410210319 <6410210319@psu.ac.th>
2023-08-15 15:25:14 +0700

    lab5

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +32,7 @@ public class EmployeeController {
 	@Autowired
 	SkillRepository skillRepository;
 
+	
 	@GetMapping("/employee")
 	public ResponseEntity<Object> getEmployee() {
 		List<Employee> employees = employeeRepository.findAll();
@@ -40,7 +43,6 @@ public class EmployeeController {
 		}
 
 	}
-
 	@GetMapping("/employee/{id}")
 	public ResponseEntity<Object> getEmployee(@PathVariable Integer id) {
 		Optional<Employee> employee = employeeRepository.findById(id);
@@ -54,55 +56,65 @@ public class EmployeeController {
 			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-
+	
 	@PostMapping("/employee")
 	public ResponseEntity<Object> addEmployee(@RequestBody Employee body) {
 		try {
+			System.out.print("hi");
 			Optional<Role> role = roleRepository.findById(4);
+			System.out.print("hi1");
 			body.setRole(role.get());
+			System.out.print("hi2");
 			Employee employee = employeeRepository.save(body);
+			System.out.print("hi3");
 			for (Skill skill : body.getSkills()) {
+				System.out.print("hi4");
 				skill.setEmployee(employee);
 				skillRepository.save(skill);
+			
 			}
+			System.out.print("hi5");
 			return new ResponseEntity<>(employee, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-
+	
+	
 	@PutMapping("/employee/{id}")
-	public ResponseEntity<Object> updatEmployee(@PathVariable Integer id, @RequestBody Employee body) {
-		Optional<Employee> employee = employeeRepository.findById(id);
-
+	public ResponseEntity<Object> updatEmployee(@PathVariable Integer id,@RequestBody Employee body) {
+		
 		try {
+			Optional<Employee> employee =employeeRepository.findById(id);
 			if (employee.isPresent()) {
 				employee.get().setFirstName(body.getFirstName());
 				employee.get().setLastName(body.getLastName());
 				employee.get().setSalary(body.getSalary());
 				employeeRepository.save(employee.get());
-				return new ResponseEntity<>(employee.get(), HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>("Client Error", HttpStatus.NOT_FOUND);
+				
+				return new ResponseEntity<>(employee,HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>("Feiled!",HttpStatus.BAD_REQUEST);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+			return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);		
+			}
 	}
-
+	
 	@DeleteMapping("/employee/{id}")
 	public ResponseEntity<Object> deleteEmployee(@PathVariable Integer id) {
-		Optional<Employee> employee = employeeRepository.findById(id);
 		try {
+			Optional<Employee> employee =employeeRepository.findById(id);
 			if (employee.isPresent()) {
 				employeeRepository.delete(employee.get());
-				return new ResponseEntity<>("Deleted", HttpStatus.NO_CONTENT);
-			} else {
-				return new ResponseEntity<>("Client Error", HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>("Deleted!",HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>("Feiled!",HttpStatus.BAD_REQUEST);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>("Internal server error",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+		
 	}
 
 }
