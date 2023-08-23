23ced221d9b0f3f9ddbaed21bdaa29818535b91f
sanphet6410210319 <6410210319@psu.ac.th>
2023-08-23 09:34:06 +0700

    exam

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,76 +2,77 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.example.demo.model.Product;
-import com.example.demo.repository.ProductRepository;
 
 @RestController
 public class ProductController {
-	@Autowired
-	ProductRepository productRepository;
-	private List<Product> data= new ArrayList<Product>();
+	private List<Product> data = new ArrayList<Product>();
 
 	@GetMapping("/product")
-	public List<Product> getProduct() {
-		return productRepository.findAll();
-	}
-	@GetMapping("/product/{id}")
-	public Optional<Product> getProduct(@PathVariable Integer id) {
-		Optional<Product> product =productRepository.findById(id);
-		return product;
-	}
-	@GetMapping("/search_text")
-	public List<Product> getProduct(@RequestParam String text) {
-		List<Product> products = new ArrayList<Product>();
-		products.add((Product) productRepository.findByProductNameContaining(text));
-		products.add((Product) productRepository.findByProductDetailContaining(text));
-		
-		return products;
+	public List<Product> getEmployee() {
+		return data;
 	}
 
 	@PostMapping("/product")
-	public Product addProduct(@RequestBody Product body) {
-		productRepository.save(body);
+	public Product addEmployee(@RequestBody Product body) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (body.getProductId() == data.get(i).getProductId()) {
+				return null;
+			}
+		}
+		data.add(body);
 		return body;
 	}
-	
-	@PutMapping("/product/{id}")
-	public Optional<Product> updatProduct(@PathVariable Integer id,@RequestBody Product body) {
-		Optional<Product> product = productRepository.findById(id);
-		if(product.isPresent()) {
-			product.get().setProductAmount(body.getProductAmount());
-			product.get().setProductDetail(body.getProductDetail());
-			product.get().setProductName(body.getProductName());
-			product.get().setProductPrice(body.getProductPrice());
-			productRepository.save(product.get());
-		}else {
-			return null;
-		}
-		return product;
-	}
-	
-	@DeleteMapping("/product/{id}")
-	public String deleteProduct(@PathVariable Integer id) {
-		Optional<Product> product = productRepository.findById(id);
-		if (product.isPresent()) {
-			productRepository.deleteById(id);
-			return "Deleted!";
-		}else {
-			return "Not found!";
-		}
-		
-	}
 
+	@GetMapping("/product/{productId}")
+	public Product getEmployeeDetail(@PathVariable Integer productId) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (productId == data.get(i).getProductId()) {
+				return data.get(i);
+			}
+		}
+
+		return null;
+	}
+	
+	@PutMapping("product/{productId}")
+	public Product updateEmployee(@PathVariable Integer productId, @RequestBody Product body) {
+		
+		for (int i = 0; i < data.size(); i++) {
+			if (productId == data.get(i).getProductId()) {	
+				data.get(i).setProductName(body.getProductName());
+				data.get(i).setProductDetail(body.getProductDetail());
+				data.get(i).setProductPrice(body.getProductPrice());
+				data.get(i).setProductAmount(body.getProductAmount());
+				
+				return data.get(i);			
+			}
+		}	
+		return null;
+
+	}
+	
+	
+	@DeleteMapping("product/{productId}")
+	public String deleteEmployee(@PathVariable Integer productId) {
+		for (int i = 0; i < data.size(); i++) {
+			if (productId == data.get(i).getProductId()) {	
+				data.remove(i);	
+				return "delete sucess";
+			}
+		}	
+		return "product not found";
+	}
 }
